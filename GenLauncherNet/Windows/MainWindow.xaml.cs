fd7384ba28b0ec7f6dfbbfa060c2613b414f46b5
AnnatarGFS <112972544+AnnatarGFS@users.noreply.github.com>
2022-10-02 16:49:19 -0500

    Removed some unused usings.

--- GenLauncherNet/Windows/MainWindow.xaml.cs
+++ GenLauncherNet/Windows/MainWindow.xaml.cs
@@ -1,29 +1,21 @@
-﻿using GenLauncherNet;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using GenLauncherNet.Utility;
 
-
-namespace GenLauncherNet
+namespace GenLauncherNet.Windows
 {
     public partial class MainWindow : Window
     {
