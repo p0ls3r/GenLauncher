f7c7ccaa5bedc2c9ff193d677c00a7519a9a2e2b
MaliceGFS <125225012+MaliceGFS@users.noreply.github.com>
2023-02-27 14:45:46 -0600

    Corrected a minor misspelling.

--- GenLauncherNet/EntryPoint.cs
+++ GenLauncherNet/EntryPoint.cs
@@ -21,7 +21,7 @@ namespace GenLauncherNet
         public const string GenLauncherModsFolderOld = "GenLauncherModifications";
         public const string LauncherImageSubFolder = "LauncherImages";
         public const string Version = "1.0.0.1 Release";
-        public const int LaunchersCountForUpdateAdverising = 25;
+        public const int LaunchersCountForUpdateAdvertising = 25;
 
         //public const string Version = "0.0.0.1 Test";
         public const string ModdedExeDownloadLink =
